c320fe0e19dc335ef85bec097d9795f83d8e1c8d
oaissnk <oaisskurdy@gmail.com>
2023-07-16 12:32:32 +0100

    server live

--- db/index.js
+++ db/index.js
@@ -6,9 +6,15 @@ require("dotenv").config({
   path: pathToCorrectEnvFile,
 });
 
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 if (!process.env.PGDATABASE) {
   throw new Error("PGDATABASE not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
