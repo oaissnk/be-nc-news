0241f54acd74db69d5cbf14741115f05ebabf924
oaissnk <oaisskurdy@gmail.com>
2023-07-16 12:41:20 +0100

    server live

--- db/index.js
+++ db/index.js
@@ -1,20 +1,20 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-const pathToCorrectEnvFile = `${__dirname}/../.env.${ENV}`;
 require("dotenv").config({
-  path: pathToCorrectEnvFile,
+  path: `${__dirname}/../.env.${ENV}`,
 });
 
-const config = {};
-
-if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
-}
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
 
 module.exports = new Pool(config);
