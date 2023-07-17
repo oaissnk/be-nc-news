ed25307b666cc651e8d9f165193706ba9bd3a6cf
oaissnk <oaisskurdy@gmail.com>
2023-07-17 09:55:17 +0100

    added express

--- db/app.test.js
+++ db/app.test.js
@@ -256,7 +256,7 @@ describe("PATCH /api/articles/:article_id", () => {
       });
   });
 });
-describe.only("DELETE /api/comments/:comment_id", () => {
+describe("DELETE /api/comments/:comment_id", () => {
   test("204 responds with no content ", () => {
     return request(app).delete("/api/comments/1").expect(204);
   });
